de960bc9f6cdd062870e4842d6440e625b35e525
DmytroNeymyrok <pochta20022002@gmail.com>
2026-04-08 22:01:33 +0300

    Home work for lesson 10 was added

---
+++ Lesson 10 (Шибениця)/Шибениця/Шибениця/Program.cs
@@ -0,0 +1,107 @@
+﻿//Написати свій власний варіант гри “Шибениця”. Це гра, де треба вгадати посимвольно зашифроване слово. Нехай слово вже буде відомо програмі. Користувач може вводити по одній літері та має обмежену кількість невірних спроб. Очікувана гра має виглядати приблизно наступним чином:
+
+//Copy code
+//Вітаємо! Спробуйте вгадати зашифроване слово! 
+//Кількість літер у слові: 6
+//Кількість можливих невірних спроб: 6
+
+//Введіть вашу літеру: у
+//Такої літери немає! Залишилось спроб: 5
+//Введіть вашу літеру: н
+//Такої літери немає! Залишилось спроб: 4
+//Введіть вашу літеру: а
+//Така літера є у слові! Позиція літери: 4,6
+//Введіть вашу літеру: о
+//Така літера є у слові! Позиція літери: 2
+//Введіть вашу літеру: с
+//Така літера є у слові! Позиція літери: 1
+//Введіть вашу літеру: з
+//Такої літери немає! Залишилось спроб: 3
+//Введіть вашу літеру: б
+//Така літера є у слові! Позиція літери: 3
+//Введіть вашу літеру: к
+
+//Вітаємо, ви вгадали слово! Зашифроване слово: собака.
+//Дякуємо за гру.
+
+using System.Collections.Generic;
+
+string secretWord = "собака";
+char[] secretArray = secretWord.ToCharArray();
+char[] currentWord = new char[secretArray.Length];
+int tryCounter = secretArray.Length;
+List <int> positions = new List<int>();  // Масив, в який записуються позиції вгаданої літери, оновлюється при кожній новій спробі
+List <char> usedLetters = new List<char>();  //Масив для збереження використаних літер
+Console.WriteLine("Вітаємо! Спробуйте вгадати зашифроване слово!");  
+Console.WriteLine($"Кількість літер у слові: {secretArray.Length}");
+Console.WriteLine($"Кількість можливих невірних спроб: {tryCounter}");
+
+for (int i=0;  i < secretArray.Length; i++) // Заповнюю зірочками слово, яке буде виводитися потім гравцю
+{
+    currentWord[i] = '*';
+}
+
+while (tryCounter > 0 && new string(currentWord).Contains('*')) //Перевіряю, що залишилися спроби і слово повністю не розгадане (наявність зірочок)
+{
+    char guess = char.ToLower(LetterInput());
+    if (usedLetters.Contains(guess))                          //Перевірка на повторний ввод літери без зміни кількості спроб
+    {
+        Console.Write($"Здається, Ви помилилися. ");
+    }
+    usedLetters.Add(guess);
+
+    bool found = false;
+    positions.Clear();
+
+    for (int i = 0; i < secretArray.Length; i++)
+    {
+        if (secretArray[i] == guess)
+        {
+            currentWord[i] = guess;     // Вгадана літера записується замість зірочок
+            positions.Add(i + 1);
+            found = true;
+        }       
+    }
+
+    if (found)
+    {
+        Console.Write($"Така літера є у слові! Позиція літери: ");
+        positions.ForEach(p => Console.Write(p + " "));
+        Console.WriteLine($"\nПоточне слово: {new string(currentWord)}");
+    }
+    else
+    {
+        tryCounter--;
+        Console.WriteLine($"Такої літери немає! Залишилось спроб: {tryCounter}");
+    }
+}
+if (!new string(currentWord).Contains('*'))
+    Console.WriteLine($"Перемога! Ви вгадали слово: {secretWord}");
+else
+    Console.WriteLine($"Програш! Загадане слово було: {secretWord}");
+
+
+
+char LetterInput()               // Метод для вводу поточної літери
+{
+    Console.Write("Введіть вашу літеру: ");
+    bool isRightInput = false;
+    char currentLetter = '\0';
+    while (!isRightInput)
+    {
+        string? input = Console.ReadLine();
+        if (!string.IsNullOrEmpty(input) && input.Length == 1 && char.IsLetter(input[0]))
+        {
+            currentLetter = input[0];
+            isRightInput = true;
+            break;
+        }
+        else
+        {
+            Console.Write("Ви ввели не літеру. Спробуйте ще раз: ");
+        }
+    }
+    return currentLetter;
+}
+
+
--- Lesson9 (Рядки)/ConsoleApp1/ConsoleApp1/Program.cs
+++ Lesson9 (Рядки)/ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,55 +2,55 @@
 //чи починається прізвище на ту ж літеру, що й ім’я. Якщо так, вивести повідомлення
 //“Прізвище починається на ту ж літеру, що і ім’я”, інакше - “Прізвище не починається на ту ж літеру, що і ім’я”.
 
-//Console.Write("Enter user's name and surname: ");
-//string? name = Console.ReadLine()?.Trim();
-//var nameParts = name.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-//string firstPart = nameParts[0];
-//string secondPart = nameParts[1];
-//if (char.ToLower(firstPart[0]) == char.ToLower(secondPart[0]))
-//{
-//    Console.WriteLine("Прізвище починається на ту ж літеру, що і ім’я");
-//}
-//else
-//{
-//    Console.WriteLine("Прізвище не починається на ту ж літеру, що і ім’я");
-//}
+Console.Write("Enter user's name and surname: ");
+string? name = Console.ReadLine()?.Trim();
+var nameParts = name.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+string firstPart = nameParts[0];
+string secondPart = nameParts[1];
+if (char.ToLower(firstPart[0]) == char.ToLower(secondPart[0]))
+{
+    Console.WriteLine("Прізвище починається на ту ж літеру, що і ім’я");
+}
+else
+{
+    Console.WriteLine("Прізвище не починається на ту ж літеру, що і ім’я");
+}
 
 
 //ДЗ 9.2. Ви розробляєте програму для генерації текстового звіту. Ваше завдання - створити текстовий звіт,
 //який містить заголовок, дату та список подій. Використайте StringBuilder, щоб зберегти пам’ять та оптимізувати 
 //об’єднання рядків. Користувач у циклi буде вводити подiї, треба додавати їх у загальний список подiй типу StringBuilder .
 
-using System.Text;
-StringBuilder events = new StringBuilder();
-bool enterEnd = false;
-events.AppendLine("EVENTS LIST: ");
-while (!enterEnd)
-{
-    Console.WriteLine("Enter event's date (or type 'end' to stop): ");
-    string? eventDate = Console.ReadLine();
-    if (eventDate?.ToLower() == "end")
-    {
-        Finish();
-        continue;
-    }
-    Console.WriteLine("Enter event's Name: (or type 'end' to stop)");
-    string? eventName = Console.ReadLine();
-    if (eventName?.ToLower() == "end")
-    {
-        Finish();
-        continue;
-    }
-    events.Append(eventDate).Append('-').AppendLine(eventName);
-}
+//using System.Text;
+//StringBuilder events = new StringBuilder();
+//bool enterEnd = false;
+//events.AppendLine("EVENTS LIST: ");
+//while (!enterEnd)
+//{
+//    Console.WriteLine("Enter event's date (or type 'end' to stop): ");
+//    string? eventDate = Console.ReadLine();
+//    if (eventDate?.ToLower() == "end")
+//    {
+//        Finish();
+//        continue;
+//    }
+//    Console.WriteLine("Enter event's Name: (or type 'end' to stop)");
+//    string? eventName = Console.ReadLine();
+//    if (eventName?.ToLower() == "end")
+//    {
+//        Finish();
+//        continue;
+//    }
+//    events.Append(eventDate).Append('-').AppendLine(eventName);
+//}
 
-Console.WriteLine(events.ToString());
+//Console.WriteLine(events.ToString());
 
-void Finish()
-{
-    enterEnd = true;
-    Console.Clear();
-}
+//void Finish()
+//{
+//    enterEnd = true;
+//    Console.Clear();
+//}
 
 //ДЗ 9.3. Опис: Ви отримали рядок, який містить декілька слів, розділених комами. Ваша мета - видалити всі пробіли з цього рядка та вивести результат.
 
